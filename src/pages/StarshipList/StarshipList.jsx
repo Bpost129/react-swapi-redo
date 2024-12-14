07e9bbbc1f62024ba5805187c319b7f36692c60a
Bpost129 <Bpost129@yahoo.com>
2024-12-14 02:10:34 -0500

    create service function to get all ships, render ships to list

--- src/pages/StarshipList/StarshipList.jsx
+++ src/pages/StarshipList/StarshipList.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react"
+import { getStarshipList } from "../../services/sw-api"
+import { Link } from "react-router-dom"
+
 const StarshipList = () => {
+  const [starships, setStarships] = useState([])
+
+  useEffect(() => {
+    const fetchStarshipList = async () => {
+      const starshipData = await getStarshipList()
+      setStarships(starshipData.results)
+    }
+    fetchStarshipList()
+  }, [])
+
   return (
     <>
-    
+      <h1>Starship List</h1>
+      {starships.map(ship =>  // vv must grab full id not just first number  vv
+        <Link to={`/starships/${ship.url[32]}`} key={ship._id}>
+          {ship.model}
+        </Link>
+      )}
     </>
   )
 }
--- src/services/sw-api.js
+++ src/services/sw-api.js
@@ -1,2 +1,6 @@
 const baseUrl = 'https://swapi.dev/api/'
 
+export async function getStarshipList() {
+  const res = await fetch(`${baseUrl}/starships`)
+  return res.json()
+}
